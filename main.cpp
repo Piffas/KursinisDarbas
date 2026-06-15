ecfc3c7898d3d767259a885da16db6eb17af65d1
Justina <justina.rasimaite@gmail.com>
2026-06-15 11:17:11 +0300

    Pridetas average results failo kurimas analizei

--- main.cpp
+++ main.cpp
@@ -24,6 +24,15 @@ struct TestResult {
     long long moves = 0;
 };
 
+struct AverageResult {
+    string algorithmName;
+    string dataType;
+    int size = 0;
+    long long averageTimeMicroseconds = 0;
+    long long averageComparisons = 0;
+    long long averageMoves = 0;
+};
+
 //Sugeneruoja atsitiktiniu skaiciu masyva
 vector<int> generateRandomData(int size) {
     vector<int> data(size);
@@ -239,6 +248,70 @@ TestResult testMergeSort(const vector<int>& originalData, const string& dataType
     return result;
 }
 
+vector<AverageResult> calculateAverageResults(const vector<TestResult>& results) {
+    vector<AverageResult> averages;
+
+    for (int i = 0; i < results.size(); i++) {
+        bool found = false;
+
+        for (int j = 0; j < averages.size(); j++) {
+            if (averages[j].algorithmName == results[i].algorithmName &&
+                averages[j].dataType == results[i].dataType &&
+                averages[j].size == results[i].size) {
+
+                averages[j].averageTimeMicroseconds += results[i].timeMicroseconds;
+                averages[j].averageComparisons += results[i].comparisons;
+                averages[j].averageMoves += results[i].moves;
+
+                found = true;
+                break;
+                }
+        }
+
+        if (!found) {
+            AverageResult average;
+            average.algorithmName = results[i].algorithmName;
+            average.dataType = results[i].dataType;
+            average.size = results[i].size;
+            average.averageTimeMicroseconds = results[i].timeMicroseconds;
+            average.averageComparisons = results[i].comparisons;
+            average.averageMoves = results[i].moves;
+
+            averages.push_back(average);
+        }
+    }
+
+    for (int i = 0; i < averages.size(); i++) {
+        averages[i].averageTimeMicroseconds /= REPEAT_COUNT;
+        averages[i].averageComparisons /= REPEAT_COUNT;
+        averages[i].averageMoves /= REPEAT_COUNT;
+    }
+
+    return averages;
+}
+
+void saveAverageResultsToCSV(const vector<AverageResult>& averages, const string& fileName) {
+    ofstream file(fileName);
+
+    if (!file) {
+        cout << "Klaida: nepavyko sukurti vidurkiu failo." << endl;
+        return;
+    }
+
+    file << "Algorithm,DataType,Size,AverageTimeMicroseconds,AverageComparisons,AverageMoves\n";
+
+    for (int i = 0; i < averages.size(); i++) {
+        file << averages[i].algorithmName << ","
+             << averages[i].dataType << ","
+             << averages[i].size << ","
+             << averages[i].averageTimeMicroseconds << ","
+             << averages[i].averageComparisons << ","
+             << averages[i].averageMoves << "\n";
+    }
+
+    file.close();
+}
+
 int main() {
     vector<int> sizes = {5000, 10000, 50000};
     vector<TestResult> results;
@@ -275,8 +348,12 @@ int main() {
 
     saveResultsToCSV(results, "results.csv");
 
+    vector<AverageResult> averages = calculateAverageResults(results);
+    saveAverageResultsToCSV(averages, "average_results.csv");
+
     cout << "Eksperimentas baigtas." << endl;
-    cout << "Rezultatai issaugoti faile results.csv" << endl;
+    cout << "Visi rezultatai issaugoti faile results.csv" << endl;
+    cout << "Vidurkiai issaugoti faile average_results.csv" << endl;
 
     return 0;
 }
